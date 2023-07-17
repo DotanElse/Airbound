b3da83911c1a2c16ce359b806fdab2f64fbfc18f
DotanElse <dotanelse@gmail.com>
2023-07-17 18:47:04 +0300

    Libgdx setup

---
+++ ios/src/com/airbound/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.airbound.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.airbound.game.Airbound;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Airbound(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
